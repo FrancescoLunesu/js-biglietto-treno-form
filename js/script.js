8deaba5aea5fa4fc134a6a25fc145807d1a4c201
FrancescoLunesu <francesco.lunesu@outlook.it>
2020-07-16 18:09:20 +0200

    Primo commit - creazione prima struttura HTML/CSS + prove

---
+++ js/script.js
@@ -0,0 +1,52 @@
+function genera(){
+    var  x = document.getElementById('nome').value;
+    document.getElementById('cliente').innerHTML = x;
+    var y = 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+// var chilometri = parseInt(prompt("Quanti chilometri devi percorrere?"));
+// var eta = parseInt(prompt("Quanti anni ha il passeggero?"));
+// var prezzoFisso = 0.21;
+// var prodotto = prezzoFisso * chilometri;
+// var sconto = 0;
+// var prezzoFinale = prodotto;
+//
+// // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// // va applicato uno sconto del 20% per i minorenni
+// // va applicato uno sconto del 40% per gli over 65
+//
+// if (eta < 18) {
+//     sconto = (prodotto * 20) / 100;
+//     prezzoFinale = prodotto - sconto;
+//     document.getElementById('prezzo').innerHTML = "il prezzo da pagare è:" + " " + prezzoFinale;
+//
+// } else if (eta >= 65) {
+//     sconto = (prodotto * 40) / 100;
+//     prezzoFinale = prodotto - sconto;
+//     document.getElementById('prezzo').innerHTML = "il prezzo da pagare è:" + " " + prezzoFinale;
+//
+// } else {
+//     document.getElementById('prezzo').innerHTML = "il prezzo da pagare è:" + " " + prezzoFinale;
+//
+// }
